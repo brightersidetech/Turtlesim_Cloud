35c537eb3397d5d1c77d4d1fc1cfdd3fb9e1d06a
brightersidetech <brightersidetech@gmail.com>
2022-07-06 17:41:07 +0300

    first edit of readMe file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Turtlesim_Cloud
 This implementation controls the motion of the turtlesim via cloud. The implementation uses FIROS to connect the turtlesim to the cloud. Together with the FIWARE context Brocker, turtlesim topics ca be subscribed to and data stored on the cloud. Data can as well be downoaded from the cloud and use to update turtlesim parameters by publishing to certain topics to them
+
+
+## Installing the required services
+### 1. Instal and configure ROS and Turtlesim
+his application needs a ROS installation. For the full tutorial on how to install ROS [use the link here](http://wiki.ros.org/noetic/Installation)
---
+++ scripts/api_client.py
@@ -0,0 +1,130 @@
+
+import requests
+import json
+import time
+
+server = "http://localhost:1026/v2/op/update"
+
+# get all entities
+def get_status():
+    r = requests.get(server)
+    # Use a breakpoint in the code line below to debug your script.
+    print(r.status_code)  # Press Ctrl+F8 to toggle the breakpoint.
+    print(r.headers)
+    print(r.content)
+    my_json = json.loads(r.content)
+    print(my_json)
+
+# Create  entity
+def create():
+    data = {"id": "urn:ngsi-ld:Store:004",
+            "type": "Store",
+            "address": {
+                "type": "PostalAddress",
+                "value": {
+                    "streetAddress": "Bornholmer Straße 65",
+                    "addressRegion": "Berlin",
+                    "addressLocality": "Prenzlauer Berg",
+                    "postalCode": "10439"
+                },
+                "metadata": {
+                    "verified": {
+                        "value": "true",
+                        "type": "Boolean"
+                    }
+                }
+            },
+            "location": {
+                "type": "geo:json",
+                "value": {
+                    "type": "Point",
+                    "coordinates": [13.3986, 52.5547]
+                 }
+            },
+            "name": {
+                "type": "Text",
+                "value": "Bösebrücke Einkauf"
+            }
+        }
+    headers = {"Content-Type": "application/json"}
+    r = requests.post(server, data=json.dumps(data), headers=headers)
+    print(r.status_code)
+    request_status("post", r.status_code)
+
+# display request status message
+def request_status(method, code):
+    if method == "post" and code == 201:
+        print("Entity has been created")
+
+def update():
+    data =  {"actionType": "update",
+            "entities": [ 
+            {
+                    "id": ".turtle1.cmd_vel",
+                    "type": "geometry_msgs%2FTwist",
+                    "angular": {
+                        "type": "geometry_msgs%2FVector3",
+                        "value": {
+                            "x": {
+                                "value": 0,
+                                "type": "number"
+                            },
+                            "y": {
+                                "value": 0,
+                                "type": "number"
+                            },
+                            "z": {
+                                "value": 1,
+                                "type": "number"
+                            }
+                        },
+                        "metadata": {
+                            "dataType": {
+                                "type": "dataType",
+                                "value": {
+                                    "x": "float64",
+                                    "y": "float64",
+                                    "z": "float64"
+                                }
+                            }
+                        }
+                    },
+                    "linear": {
+                        "type": "geometry_msgs%2FVector3",
+                        "value": {
+                            "x": {
+                                "value": 1,
+                                "type": "number"
+                            },
+                            "y": {
+                                "value": 0,
+                                "type": "number"
+                            },
+                            "z": {
+                                "value": 0,
+                                "type": "number"
+                            }
+                        },
+                        "metadata": {
+                            "dataType": {
+                                "type": "dataType",
+                                "value": {
+                                    "x": "float64",
+                                    "y": "float64",
+                                    "z": "float64"
+                                }
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    headers = {"Content-Type": "application/json"}
+    r = requests.post(server, data=json.dumps(data), headers=headers)
+    print(r.status_code)
+    #request_status("post", r.status_code)
+
+
+if __name__ == '__main__':
+    #get_status()
+    update()
